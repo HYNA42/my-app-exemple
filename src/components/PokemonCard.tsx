88ab76bc6ff54ca25b9ba9c8fbb33bc943b4f81b
Amadou <bylo@duck.com>
2024-10-09 00:12:20 +0200

    quete 4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,16 +1,7 @@
-const PokemonCard = () => {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+const PokemonCard = ({ pokemon }) => {
+  //   console.log(props);
 
-  const pokemon = pokemonList[1];
+  //   const pokemon = { name: "mew", imgSrc: null };
 
   return (
     <>
